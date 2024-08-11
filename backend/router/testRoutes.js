5da7fec8953ee5ad0564149322cf7f4b1b1ea618
shaheen950 <ibraackerman95@gmail.com>
2024-08-11 21:36:19 +0300

    update V2

--- backend/router/testRoutes.js
+++ backend/router/testRoutes.js
@@ -11,7 +11,8 @@ const multer = require("multer")
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         
-      cb(null, "../frontend/src/images")
+      cb(null, "../backend/images")
+
     },
     
     filename: function (req, file, cb) {
