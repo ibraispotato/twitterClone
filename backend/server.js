5483f4151ebf1bb19a362231170f77f61dab2798
shaheen950 <ibraackerman95@gmail.com>
2024-08-11 19:22:35 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({}))
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('images'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('images'));
 
 
 app.use((req, res, next) => {
