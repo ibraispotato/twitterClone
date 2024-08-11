bc2fc917ee89be8b1e06b8fec4a8a9f9ea39b413
shaheen950 <ibraackerman95@gmail.com>
2024-08-11 18:25:02 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('images'));
-app.use(`/images`, express.static(`images`));
+// app.use(`/images`, express.static(`images`));
 
 
 app.use((req, res, next) => {
