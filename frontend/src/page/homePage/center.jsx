eafea2906db5b0b557b5de3581282ac2d9a9bc0f
shaheen950 <ibraackerman95@gmail.com>
2024-08-12 12:02:06 +0300

    z

--- frontend/src/page/homePage/center.jsx
+++ frontend/src/page/homePage/center.jsx
@@ -64,7 +64,7 @@ const Center = () => {
     formdata.append('Text', Text)
     formdata.append('photo', videoFile)
     // const work = {Text}
-    const response = await fetch(`https://twitter-clone-mauve-delta.vercel.app/clone/texts`, {
+    const response = await fetch(`https://twitter-clone-mauve-delta.vercel.app/clone/texts/lsgo`, {
       method: 'POST',
       body: formdata,
       headers: {
