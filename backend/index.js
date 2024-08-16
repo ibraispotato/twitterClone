f38b38761f3203c8d9a6c963ef7a0f71c616b3bb
shaheen950 <ibraackerman95@gmail.com>
2024-08-17 00:36:15 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,17 +15,13 @@ const path = require('path');
 const app = express()
 // fs.readFileSync(`${__dirname}\\file`);
 app.use(express.json())
-app.use(cors({
-    origin: '*', // Adjust this to restrict allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     next();
-//   });
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+  });
    
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.static('images'));
