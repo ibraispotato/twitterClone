1bdbefb865347ecdff50d516b4c79e16b138fbb9
shaheen950 <ibraackerman95@gmail.com>
2024-08-16 23:37:28 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,7 @@ const path = require('path');
 const app = express()
 // fs.readFileSync(`${__dirname}\\file`);
 app.use(express.json())
-app.use(cors({
-    origin:"https://twitterclone-1-0x4h.onrender.com"
-}))
+app.use(cors())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
