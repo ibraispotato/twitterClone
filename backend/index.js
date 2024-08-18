d3a8230af739aafc27fa2712b895f6c9c459aef6
shaheen950 <ibraackerman95@gmail.com>
2024-08-18 20:30:07 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,9 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.static('images'));
